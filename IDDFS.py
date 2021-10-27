e03c908335c720b3a4cbb54ee699b6fd6997214a
DanWillisUni <dan.willis.uni@gmail.com>
2021-10-27 10:36:57 +0100

    removed main

--- IDDFS.py
+++ IDDFS.py
@@ -28,7 +28,7 @@ def dfs_rec(currentState,path:list,goalState,maxDepth):
                     return solution
     return None        
 
-def DFSgo(start,goal):
+def go(start,goal):
     print("{0} to {1}".format(start,goal))
     found = False
     depth = 1
@@ -42,3 +42,23 @@ def DFSgo(start,goal):
             depth = depth + 1
     t1 = time.time()  
     printFinal(t1-t0,solution)
+    
+a = [[0, 0, [[0, 7, 1], [4, 3, 2], [8, 6, 5]]],
+[0, 2, [[5, 6, 0], [1, 3, 8], [4, 7, 2]]],
+[2, 0, [[3, 5, 6], [1, 2, 7], [0, 8, 4]]],
+[1, 1, [[7, 3, 5], [4, 0, 2], [8, 1, 6]]],
+[2, 0, [[6, 4, 8], [7, 1, 3], [0, 2, 5]]]]
+goala = [0, 2, [[3, 2, 0], [6, 1, 8], [4, 7, 5]]]
+
+b = [[0, 0, [[0, 1, 8], [3, 6, 7], [5, 4, 2]]],
+[2, 0, [[6, 4, 1], [7, 3, 2], [0, 5, 8]]],
+[0, 0, [[0, 7, 1], [5, 4, 8], [6, 2, 3]]],
+[0, 2, [[5, 4, 0], [2, 3, 1], [8, 7, 6]]],
+[2, 1, [[8, 6, 7], [2, 5, 4], [3, 0, 1]]]]
+goalb = [2,2,[[1,2,3],[4,5,6],[7,8,0]]]
+
+for i in a:
+    go(i,goala)
+for i in b: 
+    go(i,goalb)
+print("Done")
